28ae2e4fdbdcaafc01bbe7762a1d6d76ecd0b346
Bima Bayu <bimabayuputraa@gmail.com>
2024-09-02 23:30:37 +0700

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,29 +2,47 @@ import streamlit as st
 import time
 import os
 from query import kueri
+from PyPDF2 import PdfReader, PdfWriter
+from streamlit_pdf_viewer import pdf_viewer
+import openai
 
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 stream = []
+_SOURCE_ = "Awal"
 # Streamed response emulator
 def query_manager(prompt, database_path):
     hasil_kueri = kueri(prompt, database_path)
     if hasil_kueri == None:
-        formatted_response = "Tidak dapat menemukan hasil yang cocok."
+        return None
     else:
         output_response, output_source = hasil_kueri
-        i = 1
-        # output_response = 'Zayed Balbahaith dari College of Technological Innovation melakukan penelitian dengan judul "Feature-based Approach" dan rilisnya pada bulan September 2021'
-        # output_source = ['data/books/Detection of SQL Injection Attacks.pdf', 'data/books/SQL Injection Vulnerability Detection Using Deep Learning A Feature-based Approach.pdf', 'data/books/Detection of SQL Injection Attacks.pdf', 'data/books/Detection of SQL Injection Attacks.pdf', 'data/books/Detection of SQL Injection Attacks.pdf']
         my_list = list(set(output_source))
-        formatted_response = f"{output_response}  \n  \nSumber:  \n"
-        for s in my_list:
-            src = f"{i}. {s}  \n"
-            formatted_response = formatted_response + src
-
-    return formatted_response
+        return output_response, my_list
+        # i = 1
+        # # output_response = 'Zayed Balbahaith dari College of Technological Innovation melakukan penelitian dengan judul "Feature-based Approach" dan rilisnya pada bulan September 2021'
+        # # output_source = ['data/books/Detection of SQL Injection Attacks.pdf', 'data/books/SQL Injection Vulnerability Detection Using Deep Learning A Feature-based Approach.pdf', 'data/books/Detection of SQL Injection Attacks.pdf', 'data/books/Detection of SQL Injection Attacks.pdf', 'data/books/Detection of SQL Injection Attacks.pdf']
+        # my_list = list(set(output_source))
+        # formatted_response = f"{output_response}  \n  \nSumber:  \n"
+        # for s in my_list:
+        #     src = f"{i}. {s}  \n"
+        #     formatted_response = formatted_response + src
 
 
 def response_generator(input, database_path):
-    response = query_manager(input, database_path)
+    _rsp = query_manager(input, database_path)
+    if _rsp != None:
+        _resp = _rsp[0]
+        _src = _rsp[1:]
+        source = _src[0]
+        response = f"{_resp}  \n  \nSumber:  \n"
+        i = 1
+        for s in source:
+            src = f"{i}. {s}  \n"
+            response = response + src
+        _SOURCE_ = source
+    else:
+        response = "Tidak dapat menemukan hasil yang cocok."
+        _SOURCE_ = "hehe"
     stream.append(response)
     for word in response.split(" "):
         yield word + " "
@@ -32,6 +50,7 @@ def response_generator(input, database_path):
 
 
 st.title("PDF Searcher")
+st.write(_SOURCE_)
 BASE_DATABASE_PATH = "database/"
 arr = os.listdir(BASE_DATABASE_PATH)
 
@@ -41,7 +60,7 @@ option = st.selectbox(
 )
 DATABASE_PATH = os.path.join(BASE_DATABASE_PATH, option)
 
-# Initialize chat history
+# # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
@@ -61,5 +80,7 @@ if prompt := st.chat_input("What is up?"):
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
         response = st.write_stream(response_generator(prompt, DATABASE_PATH))
+        if st.button("Stream data"):
+            _SOURCE_ = "BANGSAT"
     # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+    st.session_state.messages.append({"role": "assistant", "content": response}) 
