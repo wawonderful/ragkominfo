fa9ad3e9b80d9f6b4c47f7aed3c98e9680cde249
Bima Bayu Putra <bimabayuputraa@gmail.com>
2024-09-04 12:57:32 +0700

    create database py

--- pages/Create Database.py
+++ pages/Create Database.py
@@ -10,7 +10,6 @@ if "openai_api" not in st.session_state:
     st.switch_page("init.py")
 else:
     if include.check_openai_api_key(st.session_state.openai_api):
-        st.success("API Benar!")
         os.environ['OPENAI_API_KEY'] = st.session_state.openai_api
         with st.sidebar:
             selected = option_menu(
@@ -22,10 +21,27 @@ else:
                 st.switch_page("pages/Query.py")
             elif selected == "Create Database":
                 pass
+            elif selected == "PDF Viewer":
+                st.switch_page("pages/PDF Viewer.pdf")
 
         DATABASE_PATH = "database"
         DATA_PATH = "data"
         st.header("Create Database")
+        with st.form("create_database_form", clear_on_submit=True):
+            form_database_name = st.text_input(label="Database Name", value="", max_chars=25, key="zzz")
+            form_chunk_size = st.slider('Chunk Size', min_value=1, max_value=4000, value=500, step=100)
+            form_chunk_overlap = st.slider('Chunk Overlap', min_value=1, max_value=1000, value=500, step=50)
+            submit_btn = st.form_submit_button('Create Database')
+
+        if submit_btn:
+            chroma_path = os.path.join(DATABASE_PATH, form_database_name)
+            if os.path.exists(chroma_path):
+                form_warning = "Error, database with the same name exists"
+                st.error(form_warning)
+            else:
+                include.generate_data_store(form_database_name, form_chunk_size, form_chunk_overlap, chroma_path)
+                form_warning = f"Database Successfully Created  \nDatabase Name: {form_database_name}  \nChunk Overlap: {form_chunk_overlap}  \nLocation: {chroma_path}"
+                st.warning(form_warning)
     else:
         st.error("API Error!  \nRedirecting to Initialize OpenAI API Page")
         time.sleep(6)
