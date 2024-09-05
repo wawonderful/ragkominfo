14f38806a0e495870da8ebcf56e2dccd50862dca
ReyhandraKahfi <128920985+ReyhandraKahfi@users.noreply.github.com>
2024-09-05 12:35:14 +0700

    Update init.py

--- init.py
+++ init.py
@@ -5,17 +5,22 @@ import io
 import time
 import openai
 import gc
+import include
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.chat_models import ChatOpenAI
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.schema import Document
 from langchain_openai import OpenAIEmbeddings
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from PyPDF2 import PdfReader, PdfWriter
 from streamlit_pdf_viewer import pdf_viewer
 from langchain.evaluation import load_evaluator
 from dotenv import load_dotenv
 from langchain_ai21 import AI21SemanticTextSplitter
 from streamlit_option_menu import option_menu  
+from langchain_community.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
 
 # Initialize the session state
 if 'pdf_binary' not in st.session_state:
@@ -59,23 +64,19 @@ def pdf_navigator(pdf_binary, current_page):
 
     return current_page
 
+def query_manager(prompt):
+    hasil_kueri = include.kueri(prompt)
+    if hasil_kueri == None:
+        return None
+    else:
+        output_response, output_source = hasil_kueri
+        list_source = list(set(output_source))
+        return output_response, list_source
+
 # Load PDF to get total number of pages
 with io.BytesIO(st.session_state.pdf_binary) as fo:
     pdf_reader = PdfReader(fo)
     total_pages = len(pdf_reader.pages)
-
-# Streamed response emulator
-def query_manager(prompt):
-    i = 1
-    output_response = 'Zayed Balbahaith dari College of Technological Innovation melakukan penelitian dengan judul "Feature-based Approach" dan rilisnya pada bulan September 2021'
-    output_source = ['data/books/Detection of SQL Injection Attacks.pdf', 'data/books/SQL Injection Vulnerability Detection Using Deep Learning A Feature-based Approach.pdf', 'data/books/Detection of SQL Injection Attacks.pdf', 'data/books/Detection of SQL Injection Attacks.pdf', 'data/books/Detection of SQL Injection Attacks.pdf']
-    my_list = list(set(output_source))
-    formatted_response = f"{output_response}  \n  \nSumber:  \n"
-    for s in my_list:
-        src = f"{i}. {s}  \n"
-        formatted_response = formatted_response + src
-
-    return formatted_response
 
 def response_generator(input):
     response = query_manager(input)
@@ -87,18 +88,18 @@ def response_generator(input):
 with st.sidebar:
     selected = option_menu(
         menu_title="Navigation",
-        options=["Home", "Source PDF", "chewsday"],  # Add other options if needed
+        options=["Home", "Source PDF", "test"],  # Add other options if needed
     )
 
 if selected == "Home":
-    st.title("KOHMINFO")
+    st.title("RAG PDF Viewer")
 
 elif selected == "Source PDF":
     # PDF Navigation
     st.session_state.current_page = pdf_navigator(st.session_state.pdf_binary, st.session_state.current_page)
 
-elif selected == "chewsday":
-    st.title(f"You've got mailed {selected}")
+elif selected == "test":
+    st.title(f"You've got mailed")
 
 # Chat interface
 if selected == "Home":
@@ -120,7 +121,6 @@ if selected == "Home":
         # Display assistant response in chat message container
         with st.chat_message("assistant"):
             response = st.write_stream(response_generator(prompt))
-            st.write("GG")
         # Add assistant response to chat history
         st.session_state.messages.append({"role": "assistant", "content": response})
 
@@ -128,52 +128,48 @@ if selected == "Home":
 load_dotenv()
 
 # Set OpenAI API key
-os.environ['OPENAI_API_KEY'] = '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'
+os.environ['OPENAI_API_KEY'] = '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'
 CHROMA_PATH = "halp"
 DATA_PATH = "data/books"
 
 os.environ["AI21_API_KEY"] = '5wzoAsQLLH2UCTyxj7guYukiAcd443EQ'
 
-def generate_data_store():
+def generate_data_store(database_name, chunk_size, chunk_overlap, chroma_path):
     documents = load_documents()
-    chunks = split_text(documents)
-    save_to_chroma(chunks)
+    chunks = split_text(documents, chunk_size, chunk_overlap)
+    save_to_chroma(chunks, chroma_path)
+    chroma_path_properties = os.path.join(chroma_path, 'properties.txt')
+    f = open(chroma_path_properties, "w")
+    f.write(f"Database Name: {database_name}  \nChunk Size: {chunk_size}  \nChunk Overlap: {chunk_overlap}  \nLocation: {chroma_path}")
+    f.close()
 
 def load_documents():
     loader = PyPDFDirectoryLoader(DATA_PATH)
     documents = loader.load()
     return documents
 
-def split_text(documents: list[Document]):
-    semantic_text_splitter = AI21SemanticTextSplitter(
-        chunk_size=700,
-        chunk_overlap=100,
-        add_start_index=True
+def split_text(documents: list[Document], chunk_size, chunk_overlap):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        length_function=len,
+        add_start_index=True,
     )
-    chunks = semantic_text_splitter.split_documents(documents)
-    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
-
-    document = chunks[10]
-    print(document.page_content)
-    print(document.metadata)
+    chunks = text_splitter.split_documents(documents)
 
     return chunks
 
-def save_to_chroma(chunks: list[Document]):
+def save_to_chroma(chunks: list[Document], chroma_path):
     # Clear out the database first.
-    if os.path.exists(CHROMA_PATH):
-        shutil.rmtree(CHROMA_PATH)
+    if os.path.exists(chroma_path):
+        shutil.rmtree(chroma_path)
 
     # Create a new DB from the documents.
     db = Chroma.from_documents(
-        chunks, OpenAIEmbeddings(), persist_directory=CHROMA_PATH
+        chunks, OpenAIEmbeddings(), persist_directory=chroma_path
     )
-    # Persist is called automatically, so no need to manually call it
-    print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
+    db.persist()
     
-    # Set the db variable to None to close the connection
-    db = None
-    gc.collect()
 
 def safe_remove_directory(path, retries=3, delay=2):
     for i in range(retries):
@@ -187,18 +183,41 @@ def safe_remove_directory(path, retries=3, delay=2):
 
 # Use the function
 safe_remove_directory(CHROMA_PATH)
-  
-generate_data_store()
 
-def compare_embedding():
-    # Get embedding for a word.
+PROMPT_TEMPLATE = """
+Jawablah pertanyaan berdasarkan konteks berikut ini:
+
+{context}
+
+---
+
+
+Jawablah pertanyaan berdasarkan konteks diatas: {question}
+"""
+
+def terakhir(qwery):
+    query_text = qwery
+
+    # Prepare the DB.
     embedding_function = OpenAIEmbeddings()
-    vector = embedding_function.embed_query("apple")
-    print(f"Vector for 'apple': {vector}")
-    print(f"Vector length: {len(vector)}")
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
-    # Compare vector of two words
-    evaluator = load_evaluator("pairwise_embedding_distance")
-    words = ("apple", "iphone")
-    x = evaluator.evaluate_string_pairs(prediction=words[0], prediction_b=words[1])
-    print(f"Comparing ({words[0]}, {words[1]}): {x}")
+    # Search the DB.
+    results = db.similarity_search_with_relevance_scores(query_text, k=5)
+    if len(results) == 0 or results[0][1] < 0.7:
+        print(f"Unable to find matching results.")
+        return
+
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+    print(prompt)
+
+    model = ChatOpenAI()
+    response_text = model.predict(prompt)
+
+    sources = [doc.metadata.get("source", None) for doc, _score in results]
+    formatted_response = f"Response: {response_text}\nSources: {sources}"
+    print(formatted_response)
+
+terakhir("Apa isi article") 
